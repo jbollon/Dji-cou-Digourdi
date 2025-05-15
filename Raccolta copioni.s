b6431e2116cbf243093365d2af7a909aab83fb6d
jbollon <jbollon94@gmail.com>
2025-05-15 12:30:06 +0200

    update e rm ignored files

--- Raccolta copioni.s
+++
@@ -1,4 +0,0 @@
-\contentsline {Sound}{\hspace }{14}{figure.caption.9}
-\contentsline {Sound}{\hspace }{29}{figure.caption.14}
-\contentsline {Sound}{\hspace }{32}{figure.caption.17}
-\contentsline {Sound}{\hspace }{36}{figure.caption.19}
